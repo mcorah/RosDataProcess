3224f163c7346d6a63d162f944142de5f6b3f0e9
Micah Corah <micahcorah@gmail.com>
2018-08-24 18:36:49 -0400

    rewriting plotting example to plot all the auro kinematic data

--- src/example.jl
+++ src/example.jl
@@ -5,25 +5,48 @@ colors = generate_colors(10)
 
 println("Loading bag directory")
 bag_directory = "$(homedir())/bagfiles/decentralized_exploration_journal/"
-#file_names = readdir(bag_directory)
 bags = load_directory(bag_directory)
-
-println("Filtering bags")
-fixed = Dict("kinematic_exploration/decentralized_enabled" => false,
-             "num_robots" => 16)
-dec_bags = filter_bags(fixed, bags)
-
-bag = dec_bags[1]
 
 # show topics
 println("Topic names:")
-map(println, get_topic_names(bag))
+foreach(println, get_topic_names(bags[1]))
 
-println("Reading bags")
-entropy = read_topic("/kinematic_exploration/entropy_reduction", dec_bags;
-                     accessor = x->x[:data], interpolate = true)
+params = Dict(:num_robots => "num_robots",
+              :num_decentralized =>
+              "kinematic_exploration/num_decentralized_planning_rounds")
 
-println("Plotting data")
-plots = plot_trials(entropy, mean=true, standard_error=true, trials=false,
-                    color=colors[1])
-legend(plots[:mean], ["\$n_r=16\$"])
+ranges = map(x -> x[1] => get_range(x[2], bags), params)
+
+for num_robots = ranges[:num_robots]
+  figure()
+
+  for (ii, num_decentralized) = enumerate(ranges[:num_decentralized])
+    println("Plotting trials:"
+            * " num_robots=$(num_robots)"
+            * " num_decentralized=$(num_decentralized)")
+
+    println("  Filtering")
+    fixed = Dict(params[:num_robots] => num_robots,
+                 params[:num_decentralized] => num_decentralized)
+    trials = filter_bags(fixed, bags)
+
+    println("  Reading bags")
+    entropy = read_series("/kinematic_exploration/iteration",
+                          "/kinematic_exploration/entropy_reduction", trials;
+                          access_data=x->x[:data],
+                          access_time=x-> num_robots * x[:data],
+                          intersect=true)
+
+    println("  Plotting data")
+    plots = plot_trials(entropy, mean=true, standard_error=true,
+                        trials=false, color=colors[ii])
+
+    legend_string(n) = (n == 0 ? "G" : "D\$_$(n)\$")
+    plots[:mean][1][:set_label](legend_string(num_decentralized))
+  end
+
+  title("Entropy reduction: $(num_robots) robots")
+  xlabel("Robot-Iteration")
+  ylabel("Objective (bits)")
+  legend(loc="lower right")
+end
