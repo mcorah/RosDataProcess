eaa5962ec9cf7d25f3b370092eb44c2ac2ce439e
Micah Corah <micahcorah@gmail.com>
2018-08-29 14:24:38 -0400

    a bit of profiling code

---
+++ src/profile.jl
@@ -0,0 +1,47 @@
+# Profile data processing. Skip plotting for now.
+
+using RosDataProcess
+using ProfileView
+Profile.init(100000000, 0.001)
+
+function profile_fun()
+  println("Entering profiling code")
+
+  bag_directory = "$(homedir())/bagfiles/decentralized_exploration_journal/"
+  bags = load_directory(bag_directory)
+
+  # show topics
+  println("Topic names:")
+  foreach(println, get_topic_names(bags[1]))
+
+  params = Dict(:num_robots => "num_robots",
+                :num_decentralized =>
+                "kinematic_exploration/num_decentralized_planning_rounds")
+
+  ranges = map(x -> x[1] => get_range(x[2], bags), params)
+  for num_robots = ranges[:num_robots][1:1]
+    for (ii, num_decentralized) = enumerate(ranges[:num_decentralized][1:1])
+      fixed = Dict(params[:num_robots] => num_robots,
+                   params[:num_decentralized] => num_decentralized)
+      trials = filter_bags(fixed, bags)
+
+      entropy = read_series("/kinematic_exploration/iteration",
+                            "/kinematic_exploration/entropy_reduction", trials;
+                            access_data=x->x[:data],
+                            access_time=x-> num_robots * x[:data],
+                            intersect=true)
+    end
+  end
+
+  foreach(close, bags)
+
+  nothing
+end
+
+profile_fun()
+
+@profile foreach(1:3) do _
+  @time profile_fun()
+end
+
+ProfileView.view()
