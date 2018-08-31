b9b06abbbc8701191e8c72b1cef692a5b9348caf
Micah Corah <micahcorah@gmail.com>
2018-08-31 15:15:26 -0400

    New broadcast syntax

--- example/minimal.jl
+++ example/minimal.jl
@@ -2,8 +2,8 @@ using RosDataProcess
 
 dx = 0.1
 # Test simple time series
-a = TimeSeries(1:dx:30, sin(1:dx:30))
-b = TimeSeries(10:dx:40, cos(10:dx:40))
-c = TimeSeries(5:dx:20, tan(5:dx:20))
+a = TimeSeries(1:dx:30, sin.(1:dx:30))
+b = TimeSeries(10:dx:40, cos.(10:dx:40))
+c = TimeSeries(5:dx:20, tan.(5:dx:20))
 series = [a, b, c]
 interpolated = intersect_interpolate(series)
