4dfdea49095eb618808f60eafb6570065706f194
Micah Corah <micahcorah@gmail.com>
2018-09-07 17:47:36 -0400

    missing export

--- src/RosDataProcess.jl
+++ src/RosDataProcess.jl
@@ -19,7 +19,8 @@ mutate_time, mutate_data, map_time, map_data, indices_match
 include("time_series.jl")
 
 export normalize_start, intersect_intervals, intersect_regular, interpolate,
-get_at_time, intersect_interpolate, standard_error, differentiate
+get_at_time, intersect_interpolate, intersect_series, standard_error,
+differentiate
 
 include("data_process.jl")
 
